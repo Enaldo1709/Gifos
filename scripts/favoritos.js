6650385520452ce59d777a4d92c6f71c57488a9c
Enaldo1709 <enaldo_17@hotmail.com>
2021-06-03 15:20:14 -0500

    Inicio maquetado Single Page

---
+++ scripts/favoritos.js
@@ -0,0 +1,10 @@
+var favoritosTemplate =`<img src="../assets/icon-favoritos.svg" alt="Favoritos">
+<h2>Favoritos</h2>
+<div id="favoritos-container">
+    <!-- <div id="favoritos-empty-container" class="noVisible"> -->
+    <div id="favoritos-empty-container">
+        <img src="../assets/icon-fav-sin-contenido.svg" alt="Favoritos">
+        <p>"¡Guarda tu primer GIFO en Favoritos para que se muestre aquí!"</p>
+    </div>
+</div>`;
+
--- scripts/index.js
+++ scripts/index.js
@@ -4,6 +4,9 @@ document.addEventListener("DOMContentLoaded",()=>{
     var searchBox = document.getElementById("search-form");
     var offset =0;
 
+
+    
+
     searchBox.addEventListener("submit",(e)=>{
         e.preventDefault();
         buscar(offset,searchBox);
