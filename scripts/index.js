ded9066d71af37ed31e4c2684202083c0bec7eed
Enaldo1709 <enaldo_17@hotmail.com>
2021-06-17 09:44:50 -0500

    Se comienza la funcionalidad de la descarga de recursos

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,7 @@
 import { buscar } from "./modules/buscar.js";
 import {renderHomeStart} from "./home.js";
 import { changeTheme } from "./modules/changeTheme.js";
+import { Slider } from "./modules/trendingSlider.js";
 
 
 document.addEventListener("DOMContentLoaded",()=>{
@@ -13,7 +14,7 @@ document.addEventListener("DOMContentLoaded",()=>{
     var searchBox = document.getElementById("search-form");
     var offset =0;
 
-
+    Slider.init();
     
     document.getElementById("link-modo-oscuro").addEventListener("click",changeTheme)
     
