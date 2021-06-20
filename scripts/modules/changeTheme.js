09468c0f53360ea900bac21e9a3a9446a3e1bc93
Enaldo1709 <enaldo_17@hotmail.com>
2021-06-19 21:27:10 -0500

    Cambiar tema vista mobile

--- scripts/modules/changeTheme.js
+++ scripts/modules/changeTheme.js
@@ -1,7 +1,19 @@
 export var changeTheme = () =>{
+    const link = document.getElementById("link-modo-oscuro");
+    const linkMobile = document.getElementById("link-modo-oscuro-mobile");
+
+    if (link.innerText === "MODO NOCTURNO" && linkMobile.innerText === "Modo Nocturno"){
+        link.innerText = "MODO DIURNO";
+        linkMobile.innerText = "Modo Diurno";
+    }else{
+        link.innerText = "MODO NOCTURNO";
+        linkMobile.innerText = "Modo Nocturno";
+    }
+
+
     document.body.style.transition="0.5s ease-in-out all";
     document.body.classList.toggle("dark");
     setTimeout(()=>{
         document.body.style.transition="unset";
-    },300)
+    },500)
 }
